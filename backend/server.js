ef018f201eab8f2654b334118c913a5bb2cbd223
krishnasree76 <165487029+krishnasree76@users.noreply.github.com>
2025-12-09 14:11:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,8 @@ require("dotenv").config();  // Load environment variables
 const sendEmail = require("./emailService"); // Adjust the path as needed
 // Adjust path if needed
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
@@ -474,4 +475,4 @@ app.use((req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+
